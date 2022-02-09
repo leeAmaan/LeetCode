85ba78a50de7678b457c5baf1e7e620398520e09
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-02-09 21:48:02 +0900

    Time: 351 ms (49.44%), Space: 16.6 MB (97.83%) - LeetHub

---
+++ 561-array-partition-i/561-array-partition-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        sum = 0 
+        pair = []
+        nums.sort()
+        
+        for n in nums:
+            # 앞에서부터 오름차순으로 페어를 만들어서 합 계산
+            pair.append(n)
+            if len(pair) == 2:
+                sum += min(pair)
+                pair = []
+                
+                
+        return sum
+        
