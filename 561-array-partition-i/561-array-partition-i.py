6406bc3048b414c73ff49ebed40f2b74ae14577b
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-02-09 22:10:45 +0900

    Time: 394 ms (39.57%), Space: 16.6 MB (90.28%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.py
+++ 561-array-partition-i/561-array-partition-i.py
@@ -1,16 +1,13 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         sum = 0 
-        pair = []
         nums.sort()
         
-        for n in nums:
-            # 앞에서부터 오름차순으로 페어를 만들어서 합 계산
-            pair.append(n)
-            if len(pair) == 2:
-                sum += min(pair)
-                pair = []
-                
+        for i, n in enumerate(nums):
+            # 짝수 번째 값의 합 계산
+            if i % 2 == 0:
+                sum += n
                 
         return sum
-        
+            
+  
