1f61da8a3d002fdbc17f535de43f057fa3ccf381
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-02-13 21:04:32 +0900

    Time: 107 ms (5.07%), Space: 14.1 MB (67.91%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        # 집합으로 정열
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            # 전체 집합과 접미사 집합이 일치할 때 분리 진행
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
+        return ''
+        
