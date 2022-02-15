7eb76c00003bd33f0dde5b1234ad9315ad47d81c
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-02-15 23:10:10 +0900

    Time: 53 ms (24.56%), Space: 13.8 MB (89.04%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,12 +1,8 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        freqs = collections.defaultdict(int)
+        freqs = collections.Counter(stones) # 돌 빈도수 계산
         count = 0 
         
-        # 비교 없이 돌(stones)의 빈도 수 계산
-        for char in stones:
-            freqs[char] += 1
-            
         # 비교 없이 보석(jewels)의 빈도 수 합산
         for char in jewels:
             count += freqs[char]
