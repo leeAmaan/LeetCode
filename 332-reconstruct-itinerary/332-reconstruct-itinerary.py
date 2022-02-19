d1d48b0398ea00a2925f15222fda75081a97d935
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-02-19 17:54:23 +0900

    Time: 130 ms (41.27%), Space: 14.1 MB (97.95%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -2,19 +2,15 @@ class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         graph = collections.defaultdict(list)
         # 그래프 순서대로 구성
-        for a, b in sorted(tickets, reverse=True):
+        for a, b in sorted(tickets):
             graph[a].append(b)
             
-        route = []
-        def dfs(a):
-            # 첫 번째 값을 읽어 어휘 순 방문
-            while graph[a]:
-                dfs(graph[a].pop())
-            route.append(a)
-        
-        dfs('JFK')
+        route, stack = [], ['JFK']
+        while stack:
+            # 반복으로 스택을 구성하되 막히는 부분에서 풀어내는 처리
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop(0))
+            route.append(stack.pop())
+            
         # 다시 뒤집어 어휘 순 결과로
         return route[::-1]
-## 이 처럼 그래프를 역순으로 구성하면 추출 시에 pop()으로 처리가 가능하다. 이렇게 하면 복잡도를 
-## O(1)로 개선할 수 있다. 만일 크기가 크면 pop()과 pop(0)의 성능 차이는 O(1)과 O(n)으로 차이가 날 수있으므로
-## 주의가 꽤나 필요하다.
