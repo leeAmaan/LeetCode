5e5c49543917f6ea93dc2d237a9b67b677b6687a
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-02-21 21:05:45 +0900

    Time: 519 ms (11.01%), Space: 25.9 MB (5.00%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        from collections import defaultdict
+        import heapq
+        graph = defaultdict(set)
+        
+        for s, d, weight in flights:
+            graph[s].add((d, weight))
+            
+        #cost, cur_node, stops_in_path, visited
+        heap = [[0,src,0, set()]]
+        heapq.heapify(heap)
+        memo = set()
+        while heap:
+            cost, node, stops, visited = heapq.heappop(heap)
+            if node == dst:
+                return cost
+            if node not in graph:
+                continue
+            if stops > k:
+                continue
+            if node in visited or (node, stops) in memo:
+                continue
+            memo.add((node, stops))
+            visited.add(node)
+            for ney, weight in graph[node]:
+                heapq.heappush(heap, [cost + weight, ney, stops + 1, visited.union({node})])
+        return -1
