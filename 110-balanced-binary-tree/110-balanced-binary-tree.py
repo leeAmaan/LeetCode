5e819ac4f787f85211ac403b6d68a0626c3cdf65
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-02-25 22:28:45 +0900

    Time: 44 ms (97.84%), Space: 18.7 MB (57.40%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -11,8 +11,7 @@ class Solution:
                 return 0
             
             
-            left = check(root.left)
-            right = check(root.right)
+            left, right = check(root.left), check(root.right) 
             # 높이 차이가 나는 경우 -1, 이외에는 높이에 따라 1 증가
             if left == -1 or right == -1 or abs(left - right) > 1:
                 return -1 
