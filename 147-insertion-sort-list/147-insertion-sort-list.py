571a6ff7975ab24e1a259af510df1767c823045b
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-06 20:37:11 +0900

    Time: 203 ms (81.55%), Space: 16.6 MB (38.17%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.py
+++ 147-insertion-sort-list/147-insertion-sort-list.py
@@ -5,12 +5,15 @@
 #         self.next = next
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        cur = parent = ListNode(None)
+        # 초기값 변경
+        cur = parent = ListNode(0)
         while head:
             while cur.next and cur.next.val < head.val:
                 cur = cur.next
                 
             cur.next, head.next, head = head, cur.next, head.next
             
-            cur = parent
-        return cur.next
+            # 필요한 경우에만 cur 표인터가 되돌아가도록 처리 
+            if head and cur.val > head.val:
+                cur = parent
+        return parent.next
