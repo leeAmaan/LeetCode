1b73fc1a5f882f618564ea9badf4165859f96207
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-10 22:51:10 +0900

    Time: 244 ms (87.94%), Space: 23 MB (13.48%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,6 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        try:
-            return nums.index(target)
-        except ValueError:
-            return -1
+        def binary_search(left, right):
+            if left <= right:
+                # 자료형을 초과하지 않는 중앙 위치 계산
+                mid = left + (right - left) // 2
+                
+                if nums[mid] < target:
+                    return binary_search(mid + 1, right)
+                elif nums[mid] > target:
+                    return binary_search(left, mid - 1)
+                else:
+                    return mid
+            else:
+                return -1
+            
+        return binary_search(0, len(nums) - 1)
