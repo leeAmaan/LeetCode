4aa94ab4446bf2d7ac98a5d11e2162c1346e6504
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-13 00:54:03 +0900

    Time: 186 ms (77.07%), Space: 20.5 MB (58.91%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,3 +1,20 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        return any(target in row for row in matrix)
+        # 예외 처리
+        if not matrix:
+            return False
+        
+        # 첫 행의 맨 뒤 
+        row = 0 
+        col = len(matrix[0]) - 1
+        
+        while row <= len(matrix) - 1 and col >= 0:
+            if target == matrix[row][col]:
+                return True
+            # 타겟이 작으면 왼쪽으로 이동
+            elif target < matrix[row][col]:
+                col -= 1
+            # 타겟이 크면 아래로 이동
+            elif target > matrix[row][col]:
+                row += 1
+        return False
