4b563b8a03c630f44e3063f09436872a15a0a345
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-12 23:01:40 +0900

    Time: 8089 ms (5.03%), Space: 15 MB (27.04%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,15 +1,7 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for k, v in enumerate(numbers):
-            left, right = k + 1, len(numbers) - 1
             expected = target - v
-            # 이진 검색으로 나머지 값 판별
-            while left <= right:
-                mid = left + (right - left) // 2
-                if numbers[mid] < expected:
-                    left = mid + 1
-                elif numbers[mid] > expected:
-                    right = mid - 1
-                else:
-                    return k + 1, mid + 1
-                
+            i = bisect.bisect_left(numbers[k+1:], expected)
+            if i < len(numbers[k+1:]) and numbers[i + k + 1] == expected:
+                return k + 1, i + k + 2
