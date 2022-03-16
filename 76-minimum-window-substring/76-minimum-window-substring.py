448989f5211c65c1aa020bbefa53b0a87ee0585c
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-17 00:45:43 +0900

    Time: 3260 ms (5.01%), Space: 14.8 MB (24.16%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,23 +1,21 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need = collections.Counter(t)
-        missing = len(t)
-        left = start = end = 0
+        t_count = collections.Counter(t)
+        current_count = collections.Counter()
         
+        start = float('-inf')
+        end = float('inf')
+        
+        left = 0
         # 오른쪽 포인터 이동
         for right, char in enumerate(s, 1):
-            missing -= need[char] > 0
-            need[char] -= 1
+            current_count[char] += 1
             
-            
-            if missing == 0:
-                while left < right and need[s[left]] < 0:
-                    need[s[left]] += 1
-                    left += 1
-                    
-                if not end or right - left <= end - start:
+            # AND 연산 결과로 왼쪽 포인터 이동 판단
+            while current_count & t_count == t_count:
+                if right - left < end - start:
                     start, end = left, right
-                    need[s[left]] += 1
-                    missing += 1
-                    left += 1
-        return s[start:end]
+                current_count[s[left]] -= 1
+                left += 1
+                
+        return s[start: end] if end - start <= len(s) else ''
