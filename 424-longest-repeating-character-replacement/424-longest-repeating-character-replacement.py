a4bb7faf59b436f8860a494c718b6f12845e4b80
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-17 22:57:00 +0900

    Time: 198 ms (47.56%), Space: 14.1 MB (37.53%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,14 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left = right = 0
+        maxf = result = 0
         counts = collections.Counter()
-        for right in range(1, len(s) + 1):
-            counts[s[right - 1]] += 1
-            # 가장 흔하게 등장하는 문자 탐색
-            max_char_n = counts.most_common(1)[0][1]
+        for i in range(len(s)):
+            counts[s[i]] += 1
+            maxf = max(maxf, counts[s[i]])
+            if result - maxf < k:
+                result += 1
+            else:
+                counts[s[i - result]] -= 1
+        return result
             
-            # k 초과시 왼쪽 포인터 이동
-            if right - left - max_char_n > k:
-                counts[s[left]] -= 1
-                left += 1
-        return right - left
