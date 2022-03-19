562421ec2e598c18fa65494355d67c14d351d6a3
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-19 21:22:46 +0900

    Time: 107 ms (82.24%), Space: 14.5 MB (46.64%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -1,14 +1,26 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        heap = []
-        # 키 역순, 인덱스 삽입
-        for person in people:
-            heapq.heappush(heap, (-person[0], person[1]))
-            
-        result = []
-        # 키 역순, 인덱스 추출
-        while heap:
-            person = heapq.heappop(heap)
-            result.insert(person[1], [-person[0], person[1]])
+        if not people: 
+            return []
+
+        # obtain everyone's info
+        # key=height, value=k-value, index in original array
+        peopledct, height, result = {}, [], []
         
+        for i in range(len(people)):
+            p = people[i]
+            if p[0] in peopledct:
+                peopledct[p[0]] += (p[1], i),
+            else:
+                peopledct[p[0]] = [(p[1], i)]
+                height += p[0],
+
+        height.sort()      # here are different heights we have
+
+        # sort from the tallest group
+        for h in height[::-1]:
+            peopledct[h].sort()
+            for p in peopledct[h]:
+                result.insert(p[0], people[p[1]])
+
         return result
