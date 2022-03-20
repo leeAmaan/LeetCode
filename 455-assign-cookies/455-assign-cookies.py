4d4fdd738ec43ab06b67e64575e62a010276b133
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-20 16:07:07 +0900

    Time: 306 ms (18.24%), Space: 15.9 MB (49.98%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -4,14 +4,14 @@ class Solution:
         g.sort()
         s.sort()
         
-        child_i = cookie_j = 0
-        
-        # 만족하지 못할 때까지 그리디 진행한다.
-        while child_i < len(g) and cookie_j < len(s):
-            if s[cookie_j] >= g[child_i]:
-                child_i += 1
+        result = 0
+        for i in s:
+            # 이진 검색으로 더 큰 인덱스 탐색
+            index = bisect.bisect_right(g, i)
+            if index > result:
+                result += 1
                 
-            cookie_j += 1
-        
-        return child_i
+                
+        return result
+            
         
