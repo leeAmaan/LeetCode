d06e8b1cd1413bc5f7a6ebe2207be22ba707188d
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-23 01:14:54 +0900

    Time: 32 ms (88.80%), Space: 13.8 MB (64.34%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,5 +1,13 @@
 class Solution:
-    def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        return self.fib(n - 1) + self.fib(n - 2)
+    dp = collections.defaultdict(int)
+    
+    def fib(self, N: int) -> int:
+        if N <= 1:
+            return N
+        
+        if self.dp[N]:
+            return self.dp[N]
+        self.dp[N] = self.fib(N - 1) + self.fib(N - 2)
+        return self.dp[N]
+        
+        
