4f37c5e0def76a5127a904809433029e1648cd47
leeAmaan <83857430+leeAmaan@users.noreply.github.com>
2022-03-27 16:30:48 +0900

    Time: 40 ms (65.96%), Space: 13.9 MB (25.96%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,14 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0 
-        if len(nums) <= 2:
-            return max(nums)
+        last, now = 0, 0 
         
-        dp = collections.defaultdict()
-        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-        
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        for i in nums: last, now = now, max(last + i, now)
             
-        return dp.popitem()[1]
+        return now 
