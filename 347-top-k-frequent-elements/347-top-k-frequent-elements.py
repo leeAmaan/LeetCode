57c54fb6b22a150954c03ea3690db7a4a4991916
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-04-09 20:31:43 +0900

    Time: 182 ms (21.68%), Space: 19.8 MB (5.01%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,4 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return list(zip(*collections.Counter(nums).most_common(k)))[0]
-        
+        basket = [[] for _ in range(len(nums)+1)]
+        Count = Counter(nums).items()
+        for nums, i in Count:
+            basket[i].append(nums)
+        num_list = list(chain(*basket))
+        return num_list[::-1][:k]
