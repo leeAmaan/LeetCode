0a61400bc7012cbb8729416e4f8b12c8dbcce479
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-05-10 23:16:33 +0900

    Time: 59 ms (17.68%), Space: 13.9 MB (79.39%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -2,13 +2,12 @@ from itertools import combinations
 
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        def combs(k, n, cap):
-            if not k:
-                return [[]] * (not n)
-            return [comb + [last]
-                   for last in range(1, cap)
-                   for comb in combs(k-1, n-last, last)]
-        return combs(k, n, 10)
+        combs = [[]]
+        for _ in range(k):
+            combs = [[first] + comb
+                    for comb in combs
+                    for first in range(1, comb[0] if comb else 10)]
+        return [c for c in combs if sum(c) ==n]
         
         
         
