2760ae6d9ed67442ee458063e2b79245115c2031
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-05-14 14:40:46 +0900

    Time: 609 ms (49.05%), Space: 16.8 MB (28.70%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -7,7 +7,7 @@ class Solution:
             
         # 큐 변수: [(소요시간, 정점)]
         Q = [(0, k)]
-        dist = collections.defaultdict(int)
+        dist = {}
         
         # 우선순위 큐 최솟값 기준으로 정점까지 최단 경로 삽입
         while Q:
