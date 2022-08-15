76d8aa3dcd96c4dd30328601c863a22d4172ffcd
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-08-15 23:02:49 +0900

    Time: 7519 ms (5.89%), Space: 14.9 MB (77.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_map ={}
-        # 하나의 for 문으로 통합 
-        for i, num in enumerate(nums):
-            if target - num in nums_map:
-                return [nums_map[target - num], i]
-            nums_map[num] = i 
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j] 
+                       
 
         
      
