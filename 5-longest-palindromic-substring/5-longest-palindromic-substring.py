924c133695431d76fc54c51637073d1302a82c82
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-08-15 21:25:27 +0900

    Time: 130 ms (98.86%), Space: 14 MB (60.50%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,18 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-        for i in range(len(s)):
+        # 펠린드롬 판별 및 투 포인터 확장 
+        def expand(left: int , right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+        
+        # 해당 사항이 없을 때 빠르게 리턴
+        if len(s) < 2 or s == s[::-1]:
+            return s 
+        print(s)
+        result = ''
+        # 슬라이딩 윈도우 우측으로 이동
+        for i in range(len(s) - 1):
+            result = max(result, expand(i, i + 1), expand(i, i + 2), key=len)
             
-            temp = self.helper(s, i, i)
-            if len(temp) > len(res):
-                res = temp
-                
-            temp = self.helper(s, i, i+1)
-            if len(temp) > len(res):
-                res = temp
-        return res
-    
-    def helper(self, s, l, r):
-        while l >= 0 and r < len(s) and s[l]==s[r]:
-            l -= 1; r += 1
-        return s[l+1:r]
+        return result 
