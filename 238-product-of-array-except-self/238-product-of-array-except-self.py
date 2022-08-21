01fa892888a48e7237596ac57d124f17a23f8ffb
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-08-21 22:38:37 +0900

    Time: 521 ms (7.47%), Space: 39.3 MB (5.49%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,18 +1,37 @@
+import numpy as np 
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        out = []
+        res = []
         p = 1
-        # 왼쪽 곱셈부터
-        for i in range(0, len(nums)):
-            out.append(p)
-            p = p * nums[i]
-        p = 1
-        # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
-        for i in range(len(nums) - 1, 0 - 1, -1):
-            out[i] = out[i] * p 
-            p = p * nums[i]
         
-        return out
+        for i in range(0, len(nums)):
+            res.append(p)
+            p = p * nums[i]
+            
+        p = 1
+        
+        
+        for i in range(len(nums) -1, 0 -1, -1):
+            res[i] = res[i] * p
+            p = p*nums[i]
+            
+        return res
+        
+        
+        
+        
+        
+        
+        
+        # print(np.prod(nums))
+        # print(nums[1:])
+        
+        #for i in range(len(nums)):
+        #    print(nums.pop(i))
+        #    nums_f"{i}" = nums
+        #    del num_f"{i}"[i]
+        #    return res.append([np.prod(num_f"{i}")])
             
             
             
