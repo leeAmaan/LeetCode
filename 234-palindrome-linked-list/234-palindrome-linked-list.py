9109708f3c7e80211cedfaf323b737cca8768b6d
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-08-23 22:58:56 +0900

    Time: 2050 ms (5.01%), Space: 47 MB (21.74%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,16 +5,19 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        rev = None
-        slow = fast = head
-        # 런너를 이용해 역순 연결 리스트 구성
-        while fast and fast.next:
-            fast = fast.next.next
-            rev, rev.next, slow = slow, rev, slow.next
-        if fast:
-            slow = slow.next
+        q = []
+        
+        if not head:
+            return True
+        
+        node = head
+        
+        while node is not None:
+            q.append(node.val)
+            node = node.next
             
-        # 팰린드롬 여부 확인
-        while rev and rev.val == slow.val:
-            slow, rev = slow.next, rev.next
-        return not slow
+    
+        while len(q) > 1:
+            if q.pop(0) != q.pop():
+                return False
+        return True
