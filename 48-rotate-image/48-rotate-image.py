a67fe5315d10dd5cbd8fcb07cce5d6e1b27da027
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-08-31 00:02:01 +0900

    Time: 59 ms (41.69%), Space: 13.9 MB (29.99%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,7 +1,4 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        m = len(matrix)
-        for i in range(m//2):
-            for j in range(m-m//2):
-                matrix[i][j], matrix[~j][i], matrix[~i][~j], matrix[j][~i] = matrix[~j][i], matrix[~i][~j], matrix[j][~i], matrix[i][j] 
+        matrix[:] = map(list, zip(*matrix[::-1]))
         
