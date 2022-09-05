95448fd17d6530101a9e09257d54127580c6867e
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-09-05 20:35:08 +0900

    Time: 80 ms (52.45%), Space: 16.1 MB (10.93%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        q, res = [root], []
+        while any(q):
+            res.append([node.val for node in q])
+            #print(ret)
+            q = [child for node in q for child in node.children if child]
+            #print(q)
+        return res
+        
+        
+        
+        
+        
+        
+        
+        #left.val = 
+        #right.val = 
+        #res = []
+        #levelOrder():
+            #res.append(s)
+            
