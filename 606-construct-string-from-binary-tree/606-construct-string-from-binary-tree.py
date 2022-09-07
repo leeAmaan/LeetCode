0820ae832a0e82da2a17acfae11f069669ab7624
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-09-07 23:14:21 +0900

    Time: 106 ms (20.73%), Space: 16.5 MB (30.02%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
@@ -9,9 +9,22 @@ class Solution:
         if not root:
             return ''
         
-        left = '({})'.format(self.tree2str(root.left)) if (root.left or root.right) else ''
-        right = '({})'.format(self.tree2str(root.right)) if root.right else ''
-        return '{}{}{}'.format(root.val, left, right)
+        ans = str(root.val)
+        
+        if root.left:
+            ans += "("+ self.tree2str(root.left) +")"
+            
+        if not root.left and root.right:
+            ans += "()"
+        
+        if root.right:
+            ans += "("+ self.tree2str(root.right) +")"
+        
+        return ans
+        
+        # left = '({})'.format(self.tree2str(root.left)) if (root.left or root.right) else ''
+        # right = '({})'.format(self.tree2str(root.right)) if root.right else ''
+        # return '{}{}{}'.format(root.val, left, right)
         
         #left.val = self.tree2str(left)
         #right.val = self.tree2str(right)
