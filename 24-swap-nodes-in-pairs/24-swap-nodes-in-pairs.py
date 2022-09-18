4ac7551ffb9125f57aa0283de2fe3702577b108b
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-09-18 23:56:43 +0900

    Time: 60 ms (25.98%), Space: 13.8 MB (64.32%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -5,26 +5,51 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-        cur = head 
-        
-        while cur and cur.next:
-            # 값만 교환 
-            cur.val, cur.next.val = cur.next.val, cur.val
-            cur = cur.next.next
-            
-        return head
-        
-        
-        
-        
         if head and head.next:
             p = head.next
-            # 스왑된 값 리턴 받음
+            #print(p, "p1")
+        # 스왑된 값 리턴 받음
             head.next = self.swapPairs(p.next)
-            print(head.next)
+            #print(head.next, "head.next")
             p.next = head
-            return p 
+            #print(p.next, "p.next")
+            return p
+            #print(p, "return p")
         return head
+        
+        
+        
+        
+        
+        
+        #root = prev = ListNode(None)
+        #prev.next = head
+        #print(head.next)
+        #print(prev.next)
+        #while head and head.next:
+            # print(head, head.next)
+            # b가 a(head)를 가리키도록 할당
+         #   b = head.next
+          #  print(b)
+          #  print(head, "head1")
+          #  head.next = b.next
+          #  print(head.next)
+          #  print(head, "head2")
+          #  b.next = head
+          #  print(b.next)
+           # print(head, "head3")
+            
+            # prev가 b를 가리키도록 할당
+            #prev.next = b
+            #print(prev.next)
+            # 다음번 비교를 위해 이동 
+            #head = head.next
+            #prev = prev.next.next
+            #print(prev)
+        #return root.next
+            
+        
+        
+        
+
         
