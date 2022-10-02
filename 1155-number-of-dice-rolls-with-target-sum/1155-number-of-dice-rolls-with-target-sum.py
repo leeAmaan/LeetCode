f70852733e4f4c6d744e2333306f9c6eea025097
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-10-03 00:44:19 +0900

    Time: 1108 ms (34.09%), Space: 14.3 MB (84.81%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         dp = [1] + [0 for _ in range(target)]
-        
+        print(dp)
         for _ in range(n):
             for t in range(target, -1, -1):
                 dp[t] = sum(dp[max(0, t-k):t])
-                
+                print(dp[t], t)
+        print(dp)
         return dp[target] % (10**9 + 7)
     
     
