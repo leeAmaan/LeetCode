c23d71cb89cb97bb8fa18faf386b92cd46b3bbb8
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-10-19 21:53:08 +0900

    Time: 114 ms (49.72%), Space: 13.8 MB (94.22%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -3,12 +3,5 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         ans = Counter(words)
         ans = sorted(ans, key=lambda word:(-ans[word], word)) 
-        #list(ans.keys())
-        print(ans)
-        res = []
-        
-        for i in range(k):
-            res.append(ans[i])
-        
-        
-        return res
+        return ans[:k]
+   
