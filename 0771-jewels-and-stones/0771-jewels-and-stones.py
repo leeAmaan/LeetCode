0f281f774d1b6b21b805270e6a8e20a02ecd5a54
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2022-12-24 22:12:21 +0900

    Time: 38 ms (79.97%), Space: 13.8 MB (59.17%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freqs = {}
+        count = 0
+        
+        # 돌(S)의 빈도 수 계산
+        for char in stones:
+            if char not in freqs:
+                freqs[char] = 1
+            else:
+                freqs[char] += 1
+        
+        # 보석(J)의 빈도 수 합산 
+        for char in jewels:
+            if char in freqs:
+                count += freqs[char]
+        return count
+        
+        #return sum(s in jewels for s in stones)
+        
+        
