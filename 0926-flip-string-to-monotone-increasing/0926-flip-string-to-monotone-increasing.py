be19bc4be9396f5711f49d100bd673beaefa8062
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2023-01-17 22:55:01 +0900

    Time: 6594 ms (5.13%), Space: 325 MB (5.13%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        s = [int(i) for i in s] + [1]
+        
+        @lru_cache(None)
+        def dp(i,k):
+            if i == -1:
+                return 0
+            return min([dp(i-1,j) + int( k != s[i]) for j in range(k+1)])
+        return dp(len(s) - 1, 1)
+        
+        """
+        ans = 0 
+        s
+        s = 00110
+        """
