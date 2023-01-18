d9801dce92f392eee962f5a8dc58528702a4944e
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2023-01-18 23:27:24 +0900

    Time: 775 ms (39.93%), Space: 25.4 MB (5.16%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        n = len(nums)
+        minHeap = [(0, -1)]  # (preSum, index)
+        preSumSoFar = 0
+        ans = nums[0]
+        for i in range(2*n):
+            preSumSoFar += nums[i % n]
+            while minHeap and i - minHeap[0][1] > n:  # Remove if the index of minHeap.top() is out of range size `n`
+                heappop(minHeap)
+            preSumPrevious = minHeap[0][0]
+            ans = max(ans, preSumSoFar - preSumPrevious)
+            heappush(minHeap, (preSumSoFar, i))
+        return ans
+        
+        
+        """ans = 0 
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                ans += nums[i]
+                print(ans)
+        if ans == 0:
+            ans += min(nums)
+        
+        return ans""" 
