0beae22100f60829b952b9308f469581dfd5ea53
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2023-01-23 15:04:31 +0900

    Time: 1338 ms (32.46%), Space: 18.9 MB (90.33%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if not trust and n == 1:
+            return 1
+        cands = [0] * (n+1)
+        for elem1, elem2 in trust:
+            cands[elem2] += 1
+            cands[elem1] -= 1
+        print(cands)
+        return cands.index(n-1) if n-1 in cands else -1
+         
+        """
+        a = trust[0][1]
+         
+        if trust[a-1][1] is None:
+            return trust[0][1]
+        elif a != trust[a-1][1]:
+            return -1
+        else:
+            return trust[0][1]"""
