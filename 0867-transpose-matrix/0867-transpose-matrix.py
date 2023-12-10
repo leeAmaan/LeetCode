bcb5532dc018aa717a3096bce0a8529eb0684289
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2023-12-10 20:14:37 +0900

    Time: 73 ms (61.08%), Space: 17.3 MB (7.84%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -1,10 +1,10 @@
-import numpy as np 
+# import numpy as np 
 
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        trans_matrix = np.zeros([len(matrix[0]), len(matrix)], dtype = np.int32)
-        # [[0,0,0],[0,0,0],[0,0,0]]
+        # trans_matrix = np.zeros([len(matrix[0]), len(matrix)], dtype = np.int32)
+        ans=[[0]*len(matrix) for _ in range((len(matrix[0])))]
         for j in range(len(matrix)):
             for i in range(len(matrix[0])):
-                trans_matrix[i][j] = matrix[j][i]
-        return trans_matrix
+                ans[i][j] = matrix[j][i]
+        return ans
