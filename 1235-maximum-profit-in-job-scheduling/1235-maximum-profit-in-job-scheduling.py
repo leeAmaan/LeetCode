a03c5b6411e9b779c9676675aaa11a37df9a8ad7
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2024-01-06 22:59:06 +0900

    Time: 587 ms (81.57%), Space: 30.4 MB (66.36%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,18 +1,29 @@
-class Solution:
-    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        n = len(startTime)
-        jobs = sorted(list(zip(startTime, endTime, profit)))
+class Solution(object):
+    def jobScheduling(self, startTime, endTime, profit):
+        # Define BinarySearch function
+        def binarySearch(jobs, start):
+            lo, hi = 0, len(jobs) - 1
+            while lo <= hi:
+                mid = lo + (hi - lo) // 2
+                if jobs[mid][0] <= start:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            return hi
 
-        @lru_cache(None)
-        def dp(i):
-            if i == n: return 0
-            ans = dp(i + 1)  # Choice 1: Don't pick
+        # Create jobs array and sort them based on end times
+        jobs = sorted(zip(startTime, endTime, profit), key=lambda v: v[1])
 
-            for j in range(i + 1, n + 1):
-                if j == n or jobs[j][0] >= jobs[i][1]:
-                    ans = max(ans, dp(j) + jobs[i][2])  # Choice 2: Pick
-                    break
+        # Initialize DP Array
+        dp = [(0, 0)]  # (endTime, profit)
 
-            return ans
+        # Iterate and Update DP
+        for start, end, prof in jobs:
+            i = binarySearch(dp, start)
+            if dp[i][1] + prof > dp[-1][1]:
+                dp.append((end, dp[i][1] + prof))
 
-        return dp(0)
+        # return the result
+        return dp[-1][1]
+
+    ##so sad
