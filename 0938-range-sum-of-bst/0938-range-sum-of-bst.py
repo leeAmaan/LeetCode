f86094ed125ad96a524b64eb2765ab2085327c74
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2024-01-08 23:18:20 +0900

    Time: 137 ms (83.43%), Space: 24.4 MB (97.66%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -6,20 +6,6 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        stack, sum = [root], 0
-        #print(stack)
-        # 큐 연산을 이용해 반복 구조 BFS로 필요한 노드 탐색
-        while stack:
-            node = stack.pop(0)
-            #print(node)
-            #print(stack)
-            if node:
-                #print(node)
-                if node.val > low:
-                    stack.append(node.left)
-                if node.val < high:
-                    stack.append(node.right)
-                if low <= node.val <= high:
-                    sum += node.val
-                #print(stack)
-        return sum
+        if not root:
+            return 0
+        return (root.val if low <= root.val <= high else 0) + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
