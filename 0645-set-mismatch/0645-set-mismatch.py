3d2e9741f4fdbffb98a7218fdee73e0eb94b169a
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2024-01-22 23:03:46 +0900

    Time: 149 ms (94.83%), Space: 18.6 MB (51.72%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,6 +1,8 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-         return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]
+        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) -sum(set(nums))]
         
-        
-     
+        # right = list(range(1,len(nums)+1))        
+        # visited=set()
+        # mid = [x for x in nums if x in visited or (visited.add(x) or False)]
+        # return sorted(list(set(right)-set(nums)) + mid)
