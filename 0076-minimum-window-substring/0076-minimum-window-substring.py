da39e1f0a498644a5a645ad16118d3aca1ea9708
MinJoonLee <83857430+leeAmaan@users.noreply.github.com>
2024-02-04 18:03:48 +0900

    Time: 102 ms (66.92%), Space: 17.4 MB (63.60%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,21 +1,39 @@
+# import 
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_count = collections.Counter(t)
-        current_count = collections.Counter()
+        need = collections.Counter(t)
+        missing = len(t)
+        left = start = end = 0
         
-        start = float('-inf')
-        end = float('inf')
-        
-        left = 0
-        # 오른쪽 포인터 이동
         for right, char in enumerate(s, 1):
-            current_count[char] += 1
+            missing -= need[char] > 0 
+            need[char] -= 1
             
-            # AND 연산 결과로 왼쪽 포인터 이동 판단
-            while current_count & t_count == t_count:
-                if right - left < end - start:
-                    start, end = left, right
-                current_count[s[left]] -= 1
-                left += 1
+            if missing == 0:
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
                 
-        return s[start: end] if end - start <= len(s) else ''
+                if not end or right - left <= end -start:
+                    start, end = left, right
+                    need[s[left]] += 1
+                    missing += 1
+                    left += 1
+        return s[start:end]
+        
+        
+#         sl = list(s)
+#         st = list(t)
+        
+#         if len(st) > len(sl):
+#             return ""
+#         elif s == t :
+#             return s 
+#         else:
+#             for i in st:
+#                 st[i] 
+                
+            
+            
+            
